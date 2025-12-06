d6987fa64d5aefae903bbc1ff637f027efff838d
Eduardo Arthur <dududuedu0508@gmail.com>
2025-12-06 16:00:11 -0300

    finish project

---
+++ dist/app.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function soma(a, b) {
+    return a + b;
+}
+console.log("Hello world type!!");
+console.log(soma(10, 5));
+console.log(soma(5, 5));
+//# sourceMappingURL=app.js.map
